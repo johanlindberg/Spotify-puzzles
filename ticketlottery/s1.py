5a806f6272c2d89029feb07bab6b165170285565
Johan Lindberg <johan@pulp.se>
2011-12-23 22:03:01 +0100

    Started working on the ticket lottery.
    The first thing I did was to make sure that input parameters stay within
    range. At the moment I return False which might not be a very user friendly
    way of saying that something's wrong but it's a start.

---
+++ ticketlottery/s1.py
@@ -0,0 +1,43 @@
+## Spotify Software Puzzles - Ticket Lottery
+##
+## Given the number of people p in your group (all of which entered the
+## lottery) and the total number of people m that entered the lottery, what
+## is the probability that you will be able to get tickets for your entire
+## group?
+
+import doctest
+
+def solve(m, n, t, p):
+    """
+    1 <= m <= 1000: the total number of people who entered the lottery.
+    1 <= n <= m:    the total number of winners drawn.
+    1 <= t <= 100:  the number of tickets each winner is allowed to buy.
+    1 <= p <= m:    the number of people in your group.
+
+    # Check that input parameters are within range
+    >>> solve(0, 1, 1, 1)
+    False
+    >>> solve(1, 0, 1, 1)
+    False
+    >>> solve(1, 1, 0, 1)
+    False
+    >>> solve(1, 1, 1, 0)
+    False
+    >>> solve(1001, 1, 1, 1)
+    False
+    >>> solve(1, 2, 1, 1)
+    False
+    >>> solve(1, 1, 101, 1)
+    False
+    >>> solve(1, 1, 1, 2)
+    False
+    """
+
+    # Make sure that input parameters are within range
+    for var, min, max in [(m,1,1000), (n,1,m), (t,1,100), (p,1,m)]:
+      if var < min or var > max:
+        return False
+
+if __name__ == '__main__':
+    doctest.testmod()
+    
