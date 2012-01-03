7e60b95fda90855496e4ce8af53cac1cadafb337
Johan Lindberg <johan@pulp.se>
2012-01-03 21:44:25 +0100

    Added a failing test for solve.
    The idea is to have solve compute the smallest list possible and
    return it. The bias for 1009 and the formatting stuff will have to
    wait a bit.

---
+++ bilateralprojects/s1.py
@@ -0,0 +1,17 @@
+## Spotify Software Puzzles - Best Before
+##
+## Given a list of two-person teams, compute the smallest number of people
+## to get at least one person from each project, as well as a list of those
+## people. If possible (subject to the set of people being smallest possible),
+## the list of invitees should include your friend.
+
+import doctest
+
+def solve(teams):
+    """
+    >>> solve([(1009, 2011), (1017, 2011)])
+    [2011]
+    """
+
+if __name__ == '__main__':
+    doctest.testmod()
