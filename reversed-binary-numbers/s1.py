3ec835ca2cc57368ab446b4e37ee3bdd0da1f54c
Johan Lindberg <johan@pulp.se>
2013-06-13 11:01:06 +0200

    Added a failing test for reversed binary numbers.

---
+++ reversed-binary-numbers/s1.py
@@ -0,0 +1,17 @@
+## Spotify Software Puzzles - Reversed Binary Numbers
+##
+## Your task will be to write a program for reversing numbers in binary. For
+## instance, the binary representation of 13 is 1101, and reversing it gives
+## 1011, which corresponds to number 11.
+
+import doctest
+
+def solve(n):
+    """
+    >>> solve(0) # 0b -> 0b
+    0
+    """
+    pass
+
+if __name__ == '__main__':
+    doctest.testmod()
